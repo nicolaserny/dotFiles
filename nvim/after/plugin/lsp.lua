241f5deccb1e65ef409c768b79cc905fac900e64
nicolaserny <nicolas.erny@xcomponent.com>
2023-02-26 14:57:47 +0100

    Configure copilot

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -67,6 +67,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
+    ['<Tab>'] = vim.NIL,
+    ['<S-Tab>'] = vim.NIL,
 })
 
 lsp.setup_nvim_cmp({
