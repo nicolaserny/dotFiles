bfd1562341d69ce4e3aea6d5c668ad5c02dbd972
nicolaserny <nicolas.erny@xcomponent.com>
2023-04-02 15:12:06 +0200

    Improve harpoon key bindings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,9 +4,9 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 -- to avoid conflicts with cmp
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-e>e", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-e>h", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-e>j", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-e>k", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-e>l", function() ui.nav_file(4) end)
+-- to make it work, I have to set the left option key as esc+ in iterm2
+vim.keymap.set("n", "<M-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<M-j>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<M-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<M-l>", function() ui.nav_file(4) end)
