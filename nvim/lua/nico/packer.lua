7f42d5b9c2ab8b4fcf6b63b07e576198a15b587b
nicolaserny <nicolas.erny@xcomponent.com>
2023-10-08 14:41:12 +0200

    Fix indent blank lines plugin

--- nvim/lua/nico/packer.lua
+++ nvim/lua/nico/packer.lua
@@ -58,7 +58,7 @@ return require('packer').startup(function(use)
 
     use { "lukas-reineke/indent-blankline.nvim",
         config = function()
-            require("ibl").setup({ scope = { enabled = true, show_start = false, show_end = false } })
+            require("ibl").setup({ scope = { enabled = false, show_start = false, show_end = false } })
         end
     }
 
