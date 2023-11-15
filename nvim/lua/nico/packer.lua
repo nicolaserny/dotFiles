ebc88e95a87bfe1b711701a9c6bda10dafc07bc7
nicolaserny <nicolas.erny@xcomponent.com>
2023-11-15 08:52:29 +0100

    Update telescope + session paths

--- nvim/lua/nico/packer.lua
+++ nvim/lua/nico/packer.lua
@@ -15,8 +15,7 @@ return require('packer').startup(function(use)
     }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } }
     }
 
