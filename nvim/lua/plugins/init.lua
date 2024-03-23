77b082e84109bafcd7729df1aed63acbcc2c156d
nicolaserny <nicolaserny@gmail.com>
2024-03-23 17:16:51 +0100

    Update packages

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -94,5 +94,5 @@ return {
 
     'mfussenegger/nvim-dap',
     { "theHamsta/nvim-dap-virtual-text", dependencies = { "mfussenegger/nvim-dap" } },
-    { "rcarriga/nvim-dap-ui",            dependencies = { "mfussenegger/nvim-dap" } },
+    { "rcarriga/nvim-dap-ui",            dependencies = { "mfussenegger/nvim-dap",  "nvim-neotest/nvim-nio" } },
 }
