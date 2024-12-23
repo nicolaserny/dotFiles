d9aae5504433a23a8cc85dd12513691fe2824715
Nicolas Erny <nicolaserny@gmail.com>
2024-12-23 18:16:21 +0100

    Update LSP + remove TabbyML

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -6,15 +6,5 @@ if vim.g.location == 'home' then
         end
     }
 else
-    return {
-    "TabbyML/vim-tabby",
-    lazy = false,
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-    init = function()
-      vim.g.tabby_agent_start_command = {"npx", "tabby-agent", "--stdio"}
-      vim.g.tabby_inline_completion_trigger = "auto"
-    end,
-  }
+    return {}
 end
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,98 +24,6 @@ return {
         'j-hui/fidget.nvim',
     },
     config = function()
-        local on_attach = function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            -- fix omnisharp bug Invalid character in group name.
-            if client.name == 'omnisharp' then
-                client.server_capabilities.semanticTokensProvider = {
-                    full = vim.empty_dict(),
-                    legend = {
-                        tokenModifiers = { "static_symbol" },
-                        tokenTypes = {
-                            "comment",
-                            "excluded_code",
-                            "identifier",
-                            "keyword",
-                            "keyword_control",
-                            "number",
-                            "operator",
-                            "operator_overloaded",
-                            "preprocessor_keyword",
-                            "string",
-                            "whitespace",
-                            "text",
-                            "static_symbol",
-                            "preprocessor_text",
-                            "punctuation",
-                            "string_verbatim",
-                            "string_escape_character",
-                            "class_name",
-                            "delegate_name",
-                            "enum_name",
-                            "interface_name",
-                            "module_name",
-                            "struct_name",
-                            "type_parameter_name",
-                            "field_name",
-                            "enum_member_name",
-                            "constant_name",
-                            "local_name",
-                            "parameter_name",
-                            "method_name",
-                            "extension_method_name",
-                            "property_name",
-                            "event_name",
-                            "namespace_name",
-                            "label_name",
-                            "xml_doc_comment_attribute_name",
-                            "xml_doc_comment_attribute_quotes",
-                            "xml_doc_comment_attribute_value",
-                            "xml_doc_comment_cdata_section",
-                            "xml_doc_comment_comment",
-                            "xml_doc_comment_delimiter",
-                            "xml_doc_comment_entity_reference",
-                            "xml_doc_comment_name",
-                            "xml_doc_comment_processing_instruction",
-                            "xml_doc_comment_text",
-                            "xml_literal_attribute_name",
-                            "xml_literal_attribute_quotes",
-                            "xml_literal_attribute_value",
-                            "xml_literal_cdata_section",
-                            "xml_literal_comment",
-                            "xml_literal_delimiter",
-                            "xml_literal_embedded_expression",
-                            "xml_literal_entity_reference",
-                            "xml_literal_name",
-                            "xml_literal_processing_instruction",
-                            "xml_literal_text",
-                            "regex_comment",
-                            "regex_character_class",
-                            "regex_anchor",
-                            "regex_quantifier",
-                            "regex_grouping",
-                            "regex_alternation",
-                            "regex_text",
-                            "regex_self_escaped_character",
-                            "regex_other_escape",
-                        },
-                    },
-                    range = true,
-                }
-            end
-
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>)", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "<leader>(", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-v>", function() vim.lsp.buf.signature_help() end, opts)
-        end
-
         require("conform").setup({
             formatters_by_ft = {
                 javascript = { "prettier", lsp_format = "fallback" },
@@ -142,6 +50,24 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities())
 
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "<leader>)", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "<leader>(", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-v>", function() vim.lsp.buf.signature_help() end, opts)
+            end,
+        })
+
         require("fidget").setup({})
         require('mason').setup({})
         require('mason-lspconfig').setup({
@@ -152,7 +78,6 @@ return {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities,
-                        on_attach = on_attach
                     }
                 end,
                 ["lua_ls"] = function()
@@ -178,6 +103,87 @@ return {
                                 useFlatConfig = false,
                             }
                         },
+                    })
+                end,
+                omnisharp = function()
+                    require('lspconfig').omnisharp.setup({
+                        on_attach = function(client, bufnr)
+                            -- fix omnisharp bug Invalid character in group name.
+                            client.server_capabilities.semanticTokensProvider = {
+                                full = vim.empty_dict(),
+                                legend = {
+                                    tokenModifiers = { "static_symbol" },
+                                    tokenTypes = {
+                                        "comment",
+                                        "excluded_code",
+                                        "identifier",
+                                        "keyword",
+                                        "keyword_control",
+                                        "number",
+                                        "operator",
+                                        "operator_overloaded",
+                                        "preprocessor_keyword",
+                                        "string",
+                                        "whitespace",
+                                        "text",
+                                        "static_symbol",
+                                        "preprocessor_text",
+                                        "punctuation",
+                                        "string_verbatim",
+                                        "string_escape_character",
+                                        "class_name",
+                                        "delegate_name",
+                                        "enum_name",
+                                        "interface_name",
+                                        "module_name",
+                                        "struct_name",
+                                        "type_parameter_name",
+                                        "field_name",
+                                        "enum_member_name",
+                                        "constant_name",
+                                        "local_name",
+                                        "parameter_name",
+                                        "method_name",
+                                        "extension_method_name",
+                                        "property_name",
+                                        "event_name",
+                                        "namespace_name",
+                                        "label_name",
+                                        "xml_doc_comment_attribute_name",
+                                        "xml_doc_comment_attribute_quotes",
+                                        "xml_doc_comment_attribute_value",
+                                        "xml_doc_comment_cdata_section",
+                                        "xml_doc_comment_comment",
+                                        "xml_doc_comment_delimiter",
+                                        "xml_doc_comment_entity_reference",
+                                        "xml_doc_comment_name",
+                                        "xml_doc_comment_processing_instruction",
+                                        "xml_doc_comment_text",
+                                        "xml_literal_attribute_name",
+                                        "xml_literal_attribute_quotes",
+                                        "xml_literal_attribute_value",
+                                        "xml_literal_cdata_section",
+                                        "xml_literal_comment",
+                                        "xml_literal_delimiter",
+                                        "xml_literal_embedded_expression",
+                                        "xml_literal_entity_reference",
+                                        "xml_literal_name",
+                                        "xml_literal_processing_instruction",
+                                        "xml_literal_text",
+                                        "regex_comment",
+                                        "regex_character_class",
+                                        "regex_anchor",
+                                        "regex_quantifier",
+                                        "regex_grouping",
+                                        "regex_alternation",
+                                        "regex_text",
+                                        "regex_self_escaped_character",
+                                        "regex_other_escape",
+                                    },
+                                },
+                                range = true,
+                            }
+                        end
                     })
                 end,
             }
