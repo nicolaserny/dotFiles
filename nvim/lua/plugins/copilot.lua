19b50fce6bc575f34995fb45705a6930cbda3034
Nicolas Erny <nicolaserny@gmail.com>
2025-05-02 15:36:58 +0200

    Update avante config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -53,6 +53,8 @@ else
                 },
                 behaviour = {
                     enable_claude_text_editor_tool_mode = true,
+                    use_cwd_as_project_root = true,
+                    enable_cursor_planning_mode = true,
                 },
                 selector = {
                     provider = "telescope",
